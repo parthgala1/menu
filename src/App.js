db6d16fe7f50447d4dbc4f063121709d9dcdcc7a
parthgala1 <parth.gala1356@gmail.com>
2023-10-29 15:40:20 +0530

    Added buttons

--- src/App.js
+++ src/App.js
@@ -7,13 +7,22 @@ function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
 
+  const filterItems = (category) => {
+    if(category === 'all'){
+      setMenuItems(items)
+      return;
+    }
+    const newItems = items.filter((item)=> item.category === category)
+    setMenuItems(newItems)
+  };
+
   return <main>
     <section className='menu section'>
       <div className='title'>
         <h2>Our Menu</h2>
         <div className='underline'></div>
       </div>
-      <Categories />
+      <Categories filterItems = {filterItems} />
       <Menu items={menuItems}/>
     </section>
   </main>;
--- src/Categories.js
+++ src/Categories.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({filterItems}) => {
+
+  return <h2 className="btn-container">
+    <button className="filter-btn" onClick={()=>
+    filterItems('all')}>
+        all
+    </button>
+    <button className="filter-btn" onClick={()=>
+    filterItems('breakfast')}>
+        breakfast
+    </button>
+    </h2>
 };
 
 export default Categories;
